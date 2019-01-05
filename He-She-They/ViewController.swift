a5cf7121677ba279e0d21740f2d77c710cd833a5
Kyle Hernandez <kylehernandez@kyles-macbook-air.local>
2019-01-05 17:50:24 -0600

    add speech

--- He-She-They/ViewController.swift
+++ He-She-They/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Speech
 
 class ViewController: UIViewController {
   
@@ -18,38 +18,39 @@ class ViewController: UIViewController {
     var lastQIndex = 0
     var questionIndex = 0
     var answerCorrectSentence = ""
+    var pronounCorrect = ""
+    var userAnswer = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         askQuestion()
-     
     }
-    
     func startOver(){
         questions = QuestionList()
         askQuestion()
-        
     }
-    
     func askQuestion() {
         lastQIndex = questions.list.count - 1
         questionIndex = Int.random(in: 0...lastQIndex)
         sentenceLbl.text = questions.list[questionIndex].blankSentence
         answerCorrectSentence = questions.list[questionIndex].answerSentence
+        pronounCorrect = questions.list[questionIndex].answerCorrect
         questions.list.remove(at: questionIndex)
         pronounSeg.selectedSegmentIndex = 4
     }
-
     @IBAction func pronounSegBtn(_ sender: Any) {
         let pronounIndex = pronounSeg.selectedSegmentIndex
         
         switch pronounIndex {
         case 0:
-            sentenceLbl.text = "He"
+            userAnswer = "He"
+            checkAnswer()
         case 1:
-            sentenceLbl.text = "She"
+            userAnswer = "She"
+            checkAnswer()
         case 2:
-            sentenceLbl.text = "They"
+            userAnswer = "They"
+            checkAnswer()
         case 3:
             sentenceLbl.text = answerCorrectSentence
         case 4:
@@ -57,11 +58,9 @@ class ViewController: UIViewController {
                 let alert = UIAlertController(title: "Mission Completed!", message: "You've finished everysingle question, do you want to start over again?", preferredStyle: .alert)
                 let restartAction = UIAlertAction(title: "Restart All Questions", style: .default) { (handler) in
                     self.startOver()
-                    
                 }
                 alert.addAction(restartAction)
                 present(alert, animated: true, completion: nil)
-                
             }
             else{
                 askQuestion()
@@ -70,7 +69,24 @@ class ViewController: UIViewController {
             sentenceLbl.text = "Nothing"
             }
         }
-
+    func checkAnswer() {
+        if userAnswer == pronounCorrect{
+            print("You are so smart!")
+            sentenceLbl.text = answerCorrectSentence
+            
+        }
+        else {
+            print("Don't worry, nobody's perfect!")
+        }
     }
+    func readMe( myText: String) {
+        let utterance = AVSpeechUtterance(string: myText )
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        utterance.rate = 0.5
+        
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(utterance)
+    }
+}
 
 
