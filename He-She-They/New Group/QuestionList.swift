13c70abe51adb9819b9941165ad68705a3cd867c
Kyle Hernandez <kylehernandez@kyles-macbook-air.local>
2019-01-13 13:34:06 -0600

    clean up pic

--- He-She-They/New Group/QuestionList.swift
+++ He-She-They/New Group/QuestionList.swift
@@ -12,10 +12,21 @@ class QuestionList {
     var list = [Question]()
     
     init() {
+        list.append(Question(blank: "___ is playing the guitar.", answer: "He is playing the guitar.", word: "He" , image: "HeIsRaisingHisHand4"))
+        list.append(Question(blank: "___ is cooking.", answer: "He is cooking.", word: "He", image: "HeIsRaisingHisHand4"))
+        list.append(Question(blank: "___ are eating.", answer: "They are eating.", word: "They", image: "HeIsRaisingHisHand4"))
+        /*
+        list.append(Question(blank: "___ is kicking a ball.", answer: "He is kicking a ball.", word: "He", image: "HeIsKickingABall"))
         list.append(Question(blank: "___ is singing.", answer: "She is singing.", word: "She", image: "little-girl-singing-clipart-1"))
-        list.append(Question(blank: "___ is playing the guitar.", answer: "He is playing the guitar.", word: "He", image: "boy-plays-guitar"))
-        list.append(Question(blank: "___ are running.", answer: "They are running.", word: "They", image: "boy-plays-guitar"))
-        list.append(Question(blank: "___ is swimming.", answer: "She is swimming.", word: "She", image: "boy-plays-guitar"))
-        list.append(Question(blank: "___ is throwing a ball.", answer: "He is throwing a ball.", word: "He", image: "boy-plays-guitar"))
+        list.append(Question(blank: "___ are happy.", answer: "They are happy.", word: "They", image: "TheyAreHappy"))
+        list.append(Question(blank: "___ are holding hands.", answer: "They are holding hands.", word: "They", image: "TheyAreHoldingHands"))
+        list.append(Question(blank: "___ are jumping.", answer: "They are jumping.", word: "They", image: "TheyAreJumping"))
+        list.append(Question(blank: "___ are painting.", answer: "They are painting.", word: "They", image: "TheyArePainting"))
+        list.append(Question(blank: "___ are playing at the beach.", answer: "They are playing at the beach.", word: "They", image: "TheyArePlayingAtThePark"))
+        list.append(Question(blank: "___ are playing at the park.", answer: "They are playing at the park.", word: "They", image: "TheyArePlayingAtThePark"))
+        list.append(Question(blank: "___ are reading.", answer: "They are reading.", word: "They", image: "TheyAreReading"))
+        list.append(Question(blank: "___ is listening to music.", answer: "She is listening to music", word: "She", image: "SheIsListeningToMusic"))
+        list.append(Question(blank: "___ are swimming.", answer: "They are swimming.", word: "They", image: "TheyAreSwimming"))
+ */
     }
 }
